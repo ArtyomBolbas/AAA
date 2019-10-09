f954587918de405edb60565b71af25cb43a97835
Artyom Bolbas <artyombolbas@yandex.ru>
2019-10-09 16:46:11 +0300

    fix main method

--- src/main/java/com/bolbas/app/Runner.java
+++ src/main/java/com/bolbas/app/Runner.java
@@ -3,6 +3,6 @@ package com.bolbas.app;
 public class Runner {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("This is runner method");
 	}
 }
